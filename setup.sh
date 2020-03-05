82db058a2c60e0ce6fe248a8b257c7c7f197ffc0
phlgr <philippm.gartz@gmail.com>
2020-03-05 22:27:44 +0100

    Basic dev setup including working setup.sh

--- setup.sh
+++ setup.sh
@@ -60,3 +60,104 @@ npm i
 
 mkdir src/
 touch src/index.js
+touch src/index.scss
+
+touch webpack.config.js
+cat > webpack.config.js <<- "EOF"
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'test_dev_setup'
+    })
+  ],
+  devServer: {
+    contentBase: './dist'
+  },
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+          // Compiles Sass to CSS
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer `dart-sass`
+              implementation: require('sass')
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+};
+EOF
+
+touch .prettierrc
+cat > .prettierrc <<- "EOF"
+{
+"singleQuote": true
+}
+EOF
+
+touch .eslintrc.js
+cat > .eslintrc.js <<- "EOF"
+module.exports = {
+env: {
+browser: true,
+es6: true
+},
+plugins: ['prettier'],
+extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+globals: {
+Atomics: 'readonly',
+SharedArrayBuffer: 'readonly'
+},
+parserOptions: {
+ecmaVersion: 2018,
+sourceType: 'module'
+},
+rules: {}
+};
+EOF
+
+touch .prettierignore
+cat > .prettierignore <<- "EOF"
+dist/
+node_modules/
+EOF
+
+touch .eslintignore
+cat > .eslintignore <<- "EOF"
+dist/
+node_modules/
+webpack.config.js
+*.scss
+EOF
+
+echo '.DS_STORE' >> .gitignore
+echo '.vscode' >> .gitignore
+
+echo ">>> Change the title in webpack.config.js <<<"
+echo ">>> Change the title in package.json <<<"
