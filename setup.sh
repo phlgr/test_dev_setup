4eb3fe5978d825a0e1b03a02af976c6dd2d73c1b
phlgr <philippm.gartz@gmail.com>
2020-03-05 22:05:37 +0100

    Basic dev setup

---
+++ setup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+touch package.json
+cat > package.json <<- "EOF"
+{
+  "name": "test_dev_setup",
+  "version": "1.0.0",
+  "description": "",
+  "private": true,
+  "scripts": {
+    "test": "eslint --ext .js src/",
+    "start": "webpack-dev-server --open",
+    "build": "webpack"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/phlgr/test_dev_setup.git"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/phlgr/test_dev_setup/issues"
+  },
+  "homepage": "https://github.com/phlgr/test_dev_setup#readme",
+  "devDependencies": {
+    "clean-webpack-plugin": "^3.0.0",
+    "css-loader": "^3.4.2",
+    "eslint": "^6.8.0",
+    "eslint-config-prettier": "^6.10.0",
+    "eslint-plugin-prettier": "^3.1.2",
+    "file-loader": "^5.1.0",
+    "html-webpack-plugin": "^3.2.0",
+    "husky": "^4.2.3",
+    "lint-staged": "^10.0.8",
+    "prettier": "^1.19.1",
+    "sass": "^1.26.2",
+    "sass-loader": "^8.0.2",
+    "style-loader": "^1.1.3",
+    "webpack": "^4.42.0",
+    "webpack-cli": "^3.3.11",
+    "webpack-dev-server": "^3.10.3"
+  },
+  "husky": {
+    "hooks": {
+      "pre-commit": "lint-staged",
+      "pre-push": "npm test"
+    }
+  },
+  "lint-staged": {
+    "_.{js}": "eslint --fix",
+    "_.{js,css,scss,md,json}": "prettier --write",
+    "*.js": "eslint --cache --fix"
+  },
+  "main": ".eslintrc.js",
+  "dependencies": {}
+}
+EOF
+
+npm i 
+
+mkdir src/
+touch src/index.js
